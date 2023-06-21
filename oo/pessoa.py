609b94c0ec690e5941f9158b3e9cd92d0ee5ec9d
edimar <Filha@2008>
2023-06-21 13:30:14 -0400

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,11 +2,20 @@
 # e se hover mais palavras juntas, essas dever também começãr da mesma forma
 
 class Pessoa:
+    def __init__(self, nome = None, idade = 41):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
-        return f'ola {id(self)}'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p= Pessoa()
+    p = Pessoa('Fernandes')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Edimar'
+    print(p.nome)
+    print(p.idade)
+
+
