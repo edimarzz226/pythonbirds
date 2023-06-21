082c04f1466dfa1eabd426ff6de1f4dd7454c194
edimar <Filha@2008>
2023-06-21 07:27:33 -0400

    atualizando pessoa.py

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,9 +3,10 @@
 
 class Pessoa:
     def cumprimentar(self):
-        return 'ola'
+        return f'ola {id(self)}'
 
 if __name__ == '__main__':
     p= Pessoa()
     print(Pessoa.cumprimentar(p))
-    print(id(p.cumprimentar()))
+    print(id(p))
+    print(p.cumprimentar())
