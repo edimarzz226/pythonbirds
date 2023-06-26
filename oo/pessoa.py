2cf60bea71200e62bed9351f95a10e16c2570b6d
edimar <Filha@2008>
2023-06-26 07:17:38 -0400

    Criado atribudo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,6 +2,7 @@
 # e se hover mais palavras juntas, essas dever também começãr da mesma forma
 
 class Pessoa:
+    olhos = 2 # Criando fora do sem está dentro do metodo __int__ estamos criando atributo de classe ou default
     def __init__(self, *filhos, nome = None, idade = 41):
         self.idade = idade
         self.nome = nome
@@ -20,7 +21,15 @@ if __name__ == '__main__':
     for filho in Edimar.filhos:
         print(filho.nome)
     Edimar.sobrenome = 'Fernandes'
+    del Edimar.filhos
+    Edimar.olhos = 1 # mudando o valor do atributo no objeto Edimar o id dele altera
+    del Edimar.olhos # Aqui estamo deletando o atributo do objeto Edimar
     print(Edimar.__dict__) # vai aparecer um dicionário com os atributos que foram todos definidos acima idade, nome , filhos e o que foi criado dinamicamente sobrenome = Fernandes
     print(Isabella.__dict__) # Aqui diferente do objeto Isabella que não foi criado um atributo chamado sobrenome
+    Pessoa.olhos = 3
+    print(Pessoa.olhos) # Quantos olhos tem uma pessoa? Aqui estamos perguntando sobre o atributo de classe olhos
+    print(Edimar.olhos) # Acessando os atribuos da classe através do objeto "Edimar"
+    print(Isabella.olhos)
+    print(id(Pessoa.olhos), id(Edimar.olhos), id(Isabella.olhos)) # Buscar o id do atributo = olhos vai imprimir na tela
 
 
